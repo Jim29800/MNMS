e3d9857c7887303b75c01a7b5353ae62bae7b527
jim29800 <jbouvier1173@gmail.com>
2018-01-10 11:38:57 +0100

    ajout js navbar - suppr fichier inutile - traduction atelier button

--- web/js/navbar.js
+++ web/js/navbar.js
@@ -1,29 +1,8 @@
 $(document).ready(function () {
-    
-    var trigger = $('.hamburger'),
-        overlay = $('.overlay'),
-        isClosed = false;
-
-    trigger.click(function () {
-        hamburger_cross();
-    });
-
-    function hamburger_cross() {
-
-        if (isClosed == true) {
-            overlay.hide();
-            trigger.removeClass('is-open');
-            trigger.addClass('is-closed');
-            isClosed = false;
-        } else {
-            overlay.show();
-            trigger.removeClass('is-closed');
-            trigger.addClass('is-open');
-            isClosed = true;
-        }
-    }
-
-    $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-    });
+    $("#menu").click(function () {
+        $("#sidebar").toggle();
+    })
+    // $("#sidebar").click(function () {
+    //     $("#sidebar").hide();
+    // })
 });
