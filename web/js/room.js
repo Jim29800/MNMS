b08ed785c294509eba1a8c5dc9a345dd65919797
Jeanne <jeanne.tiramani@hotmail.fr>
2018-01-17 14:05:47 +0100

    ajout reset champs toggle rooms

--- web/js/room.js
+++ web/js/room.js
@@ -2,10 +2,12 @@ $(document).ready(function () {
     $(".room-need").hide();
     $(".room-new").hide();
     $(".room-all").hide();
-    
-    
 });
+
 $("#btn-room-new").click(function() {
+    $("form :input").val("");
+    $('input[type=checkbox]').prop('checked', false);
+    
     $("#appbundle_room_NeedPlace").prop("checked", false);
     $(".room-need").hide();    
     $(".room-new").show();
@@ -13,6 +15,9 @@ $("#btn-room-new").click(function() {
     
 })
 $("#btn-room-need").click(function () {
+    $("form :input").val("");
+    $('input[type=checkbox]').prop('checked', false);
+
     $("#appbundle_room_NeedPlace").prop("checked", true);    
     $(".room-need").show();
     $(".room-new").hide();
